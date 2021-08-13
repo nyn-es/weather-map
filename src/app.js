9e5f47a89c05a28bb83d8b93bf28441ea8cf2c5a
NANA YAA NSIAH <nynessilfie@gmail.com>
2021-08-12 23:21:56 -0500

    Implemented the unit converter

--- src/app.js
+++ src/app.js
@@ -20,7 +20,10 @@ function displayTemperature(response) {
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
     let iconElement = document.querySelector("#icon");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+    celsiusTemperature = response.data.main.temp;
+
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
     cityElement.innerHTML = response.data.name
     descriptionElement.innerHTML =response.data.weather[0].description;
     humidityElement.innerHTML =response.data.main.humidity;
@@ -42,7 +45,35 @@ function handleSubmit(event) {
     let cityInputElement = document.querySelector("#city-input");
     search(cityInputElement.value)
 }
-search("Dallas")
+
 
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
+
+function displayFahrenheitTemperature (event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    let fahrenheitTemperature = (celsiusTemperature*9)/5 + 32;
+
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+};
+
+function displayCelsiusTemperature (event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+};
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Dallas");
